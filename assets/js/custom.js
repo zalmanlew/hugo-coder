7709cfbd4b0264326c078486d62aa8f6b4194807
zalmanlew <35234925+zalmanlew@users.noreply.github.com>
2023-04-30 10:37:26 -0400

    add path

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   let ref = document.referrer || "noref";
+  let path = document.location.pathname;
   const url = "aHR0cHM6Ly9lbzhxY2gwNGlid2R1czgubS5waXBlZHJlYW0ubmV0L3phbG1hbmxldy5jb20";
-  fetch(`${atob(url)}?ref=${encodeURIComponent(ref)}`);
+  fetch(`${atob(url)}?ref=${encodeURIComponent(ref)}&path=${encodeURIComponent(path)}`);
 }, false);
